cfba9278dc2b06ecde3e54189fadc29bd6e9176d
Ivan Loseev <loseev5@gmail.com>
2025-08-30 17:20:45 +0300

    test: compact filter usage example

---
+++ neutrino/chain_test.go
@@ -0,0 +1,52 @@
+package neutrino
+
+import (
+	"testing"
+
+	"github.com/btcsuite/btcd/btcutil"
+	"github.com/btcsuite/btcd/btcutil/gcs/builder"
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/txscript"
+	"github.com/btcsuite/btcd/wire"
+	"github.com/satelliondao/satellion/config"
+	"github.com/stretchr/testify/assert"
+)
+
+const TestBlockHeight = 879614
+
+const TestAddress = "bc1pj587y3psgrlsyfsqzmgsy6yun2atpgkwzu03e4lfhm6a2juqchdqyd2g45"
+
+func TestGetCompactFilter(t *testing.T) {
+	cfg, err := config.Load()
+	if err != nil {
+		t.Fatal(err)
+	}
+	chain := NewChain(cfg)
+	err = chain.Start()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer chain.Stop()
+	blockhash, err := chain.chainService.GetBlockHash(TestBlockHeight)
+	if err != nil {
+		t.Fatal(err)
+	}
+	filter, err := chain.chainService.GetCFilter(*blockhash, wire.GCSFilterRegular)
+	if err != nil {
+		t.Fatal(err)
+	}
+	address, err := btcutil.DecodeAddress("bc1pj587y3psgrlsyfsqzmgsy6yun2atpgkwzu03e4lfhm6a2juqchdqyd2g45", &chaincfg.MainNetParams)
+	if err != nil {
+		t.Fatal(err)
+	}
+	script, err := txscript.PayToAddrScript(address)
+	if err != nil {
+		t.Fatal(err)
+	}
+	key := builder.DeriveKey(blockhash)
+	match, err := filter.Match(key, script)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.True(t, match, "Should match")
+}
